5dfa88084798d7ecc60ce011c4658da205d4b6c4
Lukas <lukoluko8@gmail.com>
2024-04-23 16:29:08 +0200

    Formatting

--- src/components/Hover.svelte
+++ src/components/Hover.svelte
@@ -13,35 +13,30 @@
 </div>
 
 <script>
-        (function () {
+    (function () {
+        const link = document.querySelectorAll('span > .hover-this');
+        const cursor = document.querySelector('.cursor');
 
-    const link = document.querySelectorAll('span > .hover-this');
-    const cursor = document.querySelector('.cursor');
+        const animateit = function (e) {
+            const span = this.querySelector('section');
+            const { offsetX: x, offsetY: y } = e,
+                { offsetWidth: width, offsetHeight: height } = this,
+            move = 25,
+            xMove = x / width * (move * 2) - move,
+            yMove = y / height * (move * 2) - move;
+            span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+            if (e.type === 'mouseleave') span.style.transform = '';
+        };
 
-    const animateit = function (e) {
-        const span = this.querySelector('section');
-        const { offsetX: x, offsetY: y } = e,
-        { offsetWidth: width, offsetHeight: height } = this,
+        const editCursor = e => {
+            const { clientX: x, clientY: y } = e;
+            cursor.style.left = x + 'px';
+            cursor.style.top = y + 'px';
+        };
 
-        move = 25,
-        xMove = x / width * (move * 2) - move,
-        yMove = y / height * (move * 2) - move;
-
-        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-        if (e.type === 'mouseleave') span.style.transform = '';
-    };
-
-    const editCursor = e => {
-        const { clientX: x, clientY: y } = e;
-        cursor.style.left = x + 'px';
-        cursor.style.top = y + 'px';
-    };
-
-    link.forEach(b => b.addEventListener('mousemove', animateit));
-    link.forEach(b => b.addEventListener('mouseleave', animateit));
-    window.addEventListener('mousemove', editCursor);
-
+        link.forEach(b => b.addEventListener('mousemove', animateit));
+        link.forEach(b => b.addEventListener('mouseleave', animateit));
+        window.addEventListener('mousemove', editCursor);
     })();
 </script>
 </body>
